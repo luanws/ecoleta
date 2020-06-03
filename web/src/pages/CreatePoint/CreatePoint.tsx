a1bf3c17b4fc248e980dea34a8bdf6a64d6dbe25
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-03 17:34:52 -0300

    api

--- web/src/pages/CreatePoint/CreatePoint.tsx
+++ web/src/pages/CreatePoint/CreatePoint.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
@@ -8,7 +8,21 @@ import api from '../../services/api'
 import './CreatePoint.css'
 import logo from '../../assets/logo.svg'
 
+interface Item {
+    id: number
+    title: string
+    image_url: string
+}
+
 const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([])
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data)
+        })
+    }, [])
+
     return (
         <div id="page-create-point">
             <header>
@@ -92,30 +106,12 @@ const CreatePoint = () => {
                 </fieldset>
 
                 <ul className="items-grid">
-                    <li className="selected">
-                        <img src="http://localhost:5000/uploads/lampadas.svg" alt="Teste" />
-                        <span>Óleo de cozinha</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:5000/uploads/lampadas.svg" alt="Teste" />
-                        <span>Óleo de cozinha</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:5000/uploads/lampadas.svg" alt="Teste" />
-                        <span>Óleo de cozinha</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:5000/uploads/lampadas.svg" alt="Teste" />
-                        <span>Óleo de cozinha</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:5000/uploads/lampadas.svg" alt="Teste" />
-                        <span>Óleo de cozinha</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:5000/uploads/lampadas.svg" alt="Teste" />
-                        <span>Óleo de cozinha</span>
-                    </li>
+                    {items.map(item =>
+                        <li key={item.id}>
+                            <img src={item.image_url} alt={item.title} />
+                            <span>{item.title}</span>
+                        </li>
+                    )}
                 </ul>
 
                 <button type="submit">
