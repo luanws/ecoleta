b32902f030c0fb35992cafd3588e450f11db5f04
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-04 20:47:13 -0300

    points

--- mobile/src/pages/Points/Points.tsx
+++ mobile/src/pages/Points/Points.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { Feather as Icon } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import MapView from 'react-native-maps'
+
+import styles from './styles'
 
 const Points = () => {
-    return <View></View>
+    const navigation = useNavigation()
+
+    function handleNavigateBack() {
+        navigation.goBack()
+    }
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={handleNavigateBack}>
+                <Icon name="arrow-left" size={20} color="#34cd79" />
+            </TouchableOpacity>
+
+            <Text style={styles.title}>Bem vindo</Text>
+            <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+            <View style={styles.mapContainer}>
+                <MapView style={styles.map}></MapView>
+            </View>
+            
+            <View style={styles.itemsContainer}>
+                <TouchableOpacity style={styles.item} onPress={() => {}}>
+
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 export default Points
--- mobile/src/pages/Points/styles.tsx
+++ mobile/src/pages/Points/styles.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
 
 const styles = StyleSheet.create({
     container: {
