f7267ef1fe13eaa777b38f58465cb19be341fdf3
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-04 19:00:27 -0300

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
+
+import Home from './src/pages/Home/Home'
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Hello World</Text>
-        </View>
+        <>
+            <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+            <Home></Home>
+        </>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
