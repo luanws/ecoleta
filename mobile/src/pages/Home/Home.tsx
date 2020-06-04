a2c7c88ce5b45630efe1b850a10479551efbc626
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-04 19:24:25 -0300

    estilos na home

--- mobile/src/pages/Home/Home.tsx
+++ mobile/src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
+
+import styles from './styles'
 
 const Home = () => {
     return (
-        <View>
-            <Text>Teste</Text>
+        <View style={styles.container}>
+            <Image source={require('../../assets/logo.png')} />
         </View>
     )
 }
+
+
 
 export default Home
