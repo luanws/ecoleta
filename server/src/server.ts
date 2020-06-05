1d3a4e971060a6e08efa2452bcd30f6c5e0f2557
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-05 15:43:15 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,8 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.use(errors)
-
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(errors)
 
 app.listen(5000)
