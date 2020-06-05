cdb2636c7dc23f2433725ef73c96af344a4add1f
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-05 02:06:14 -0300

    Update Points.tsx

--- mobile/src/pages/Points/Points.tsx
+++ mobile/src/pages/Points/Points.tsx
@@ -66,8 +66,8 @@ const Points = () => {
         navigation.goBack()
     }
 
-    function handleNavigateToDetail() {
-        navigation.navigate('Detail')
+    function handleNavigateToDetail(id: number) {
+        navigation.navigate('Detail', { point_id: id })
     }
 
     function handleSelectItem(id: number) {
@@ -105,7 +105,7 @@ const Points = () => {
                             <Marker
                                 key={String(point.id)}
                                 style={styles.mapMarker}
-                                onPress={handleNavigateToDetail}
+                                onPress={() => handleNavigateToDetail(point.id)}
                                 coordinate={{
                                     latitude: point.latitude,
                                     longitude: point.longitude
