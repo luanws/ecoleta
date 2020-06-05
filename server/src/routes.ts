9c64c04507ba2457323b224faf761ab00f63658b
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-05 13:38:26 -0300

    Update routes.ts

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import PointsController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
+import multer from 'multer'
+import multerConfig from './config/multer'
 
 const routes = express.Router()
 const pointsController = new PointsController()
 const itemsController = new ItemsController()
+const upload = multer(multerConfig)
 
 routes.get('/', (request, response) => response.send('ECOLETA'))
 
@@ -12,6 +15,6 @@ routes.get('/items', itemsController.index)
 
 routes.get('/points', pointsController.index)
 routes.get('/points/:id', pointsController.show)
-routes.post('/points', pointsController.create)
+routes.post('/points', upload.single('image'), pointsController.create)
 
 export default routes
