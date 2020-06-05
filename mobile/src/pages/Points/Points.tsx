3bdeff392931fdce51befe28578f946cdadb192f
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-04 23:48:58 -0300

    Update Points.tsx

--- mobile/src/pages/Points/Points.tsx
+++ mobile/src/pages/Points/Points.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, ScrollView } from 'react-native'
 import { Feather as Icon } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 import MapView from 'react-native-maps'
+import { SvgUri } from 'react-native-svg'
 
 import styles from './styles'
 
@@ -25,11 +26,20 @@ const Points = () => {
             <View style={styles.mapContainer}>
                 <MapView style={styles.map}></MapView>
             </View>
-            
-            <View style={styles.itemsContainer}>
-                <TouchableOpacity style={styles.item} onPress={() => {}}>
 
-                </TouchableOpacity>
+            <View style={styles.itemsContainer}>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{ paddingHorizontal: 20 }}
+                >
+                    {[1, 1, 1, 1, 1, 1].map(() =>
+                        <TouchableOpacity style={styles.item} onPress={() => { }}>
+                            <SvgUri width={42} height={42} uri="http://192.168.100.105:5000/uploads/lampadas.svg" />
+                            <Text style={styles.itemTitle}>Lâmpadas</Text>
+                        </TouchableOpacity>
+                    )}
+                </ScrollView>
             </View>
         </View>
     )
